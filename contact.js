3420efbdae48694206286ae571381e2f1f540204
WojciechSzw <110970008+WojciechSzw@users.noreply.github.com>
2023-08-16 12:54:42 +0200

    mailform-working

---
+++ contact.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("submit_btn");
+
+document
+  .getElementById("mail_form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    btn.value = "Wysyłanie...";
+
+    const serviceID = "default_service";
+    const templateID = "template_2erkvvc";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Wyślij";
+        alert("Wysłano, dziękujemy!");
+      },
+      (err) => {
+        btn.value = "Wyślij";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
