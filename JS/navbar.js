eb2736cf278ef2e83229f1f06ecd555bdc6dd8ce
WojciechSzw <110970008+WojciechSzw@users.noreply.github.com>
2023-12-06 19:44:04 +0100

    up offer

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,11 +1,29 @@
-document.getElementById("menu-button").addEventListener("click", function () {
-  let navbar = document.getElementById("nav-list");
-  let blackout = document.getElementById("blackout-body-navlist");
-  if (navbar.style.visibility === "hidden") {
-    navbar.style.visibility = "visible";
-    blackout.style.visibility = "visible";
-  } else {
-    navbar.style.visibility = "hidden";
-    blackout.style.visibility = "hidden";
+// document.getElementById("menu-button").addEventListener("click", function () {
+//   let navbar = document.getElementById("nav-list");
+//   let blackout = document.getElementById("blackout-body-navlist");
+//   if (navbar.style.visibility === "hidden") {
+//     navbar.style.visibility = "visible";
+//     blackout.style.visibility = "visible";
+//   } else {
+//     navbar.style.visibility = "hidden";
+//     blackout.style.visibility = "hidden";
+//   }
+// });
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  if (
+    target.classList.contains("menu-button") ||
+    target.classList.contains("fa-bars")
+  ) {
+    let navbar = document.querySelector("#nav-list");
+    let blackout = document.querySelector("#blackout-body-navlist");
+    if (navbar.style.visibility === "hidden") {
+      navbar.style.visibility = "visible";
+      blackout.style.visibility = "visible";
+    } else {
+      navbar.style.visibility = "hidden";
+      blackout.style.visibility = "hidden";
+    }
   }
 });
